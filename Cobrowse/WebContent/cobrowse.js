45e290b306a8dcec8ccbed1781afc35bd041b0e0
Ste Prescott <ste@cobrowse.io>
2024-05-13 10:52:07 +0100

    Update SDK URL

--- Cobrowse/WebContent/cobrowse.js
+++ Cobrowse/WebContent/cobrowse.js
@@ -1,5 +1,5 @@
 (function(w,t,c,p,s,e){p=new Promise(function(r){w[c]={client:function(){if(!s){
-s=document.createElement(t);s.src='https://cobrowsesdk.eu.ngrok.io/CobrowseIO.js';s.async=1;
+s=document.createElement(t);s.src='https://js.cobrowse.io/CobrowseIO.js';s.async=1;
 e=document.getElementsByTagName(t)[0];e.parentNode.insertBefore(s,e);s.onload=function()
 {r(w[c]);};}return p;}};});})(window,'script','CobrowseIO');
 
