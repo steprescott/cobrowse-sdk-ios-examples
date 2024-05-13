ce764cc5d127adb618902a342844796a9533149c
Ste Prescott <ste@cobrowse.io>
2024-05-13 10:21:36 +0100

    Add trustedOrigins to enable web demo

---
+++ Cobrowse/WebContent/cobrowse.js
@@ -0,0 +1,13 @@
+(function(w,t,c,p,s,e){p=new Promise(function(r){w[c]={client:function(){if(!s){
+s=document.createElement(t);s.src='https://cobrowsesdk.eu.ngrok.io/CobrowseIO.js';s.async=1;
+e=document.getElementsByTagName(t)[0];e.parentNode.insertBefore(s,e);s.onload=function()
+{r(w[c]);};}return p;}};});})(window,'script','CobrowseIO');
+
+CobrowseIO.trustedOrigins = [
+    'https://cobrowse.io'
+];   
+
+(async () => {
+    await CobrowseIO.client();
+    await CobrowseIO.start();
+})();
