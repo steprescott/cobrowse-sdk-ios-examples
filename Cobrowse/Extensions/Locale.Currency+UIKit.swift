92ab05e931cbf2f8cfc61f6db8c20baa82e55305
Ste Prescott <ste@cobrowse.io>
2025-06-04 15:09:07 +0100

    Format indentation

--- Cobrowse/Extensions/Locale.Currency+UIKit.swift
+++ Cobrowse/Extensions/Locale.Currency+UIKit.swift
@@ -8,11 +8,11 @@ import UIKit
 extension Locale.Currency {
     
     var icon: UIImage? {
-            switch identifier {
-                case "USD": return UIImage(systemName: "dollarsign.square")
-                case "EUR": return UIImage(systemName: "eurosign.square")
-                case "JPY": return UIImage(systemName: "yensign.square")
-                default: return UIImage(systemName: "sterlingsign.square")
-            }
+        switch identifier {
+            case "USD": return UIImage(systemName: "dollarsign.square")
+            case "EUR": return UIImage(systemName: "eurosign.square")
+            case "JPY": return UIImage(systemName: "yensign.square")
+            default: return UIImage(systemName: "sterlingsign.square")
         }
+    }
 }
