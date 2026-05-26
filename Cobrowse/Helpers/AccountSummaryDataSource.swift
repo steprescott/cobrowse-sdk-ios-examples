cbbedf38ed955225748d91eb55169d8f74363a50
Ste Prescott <ste@cobrowse.io>
2026-05-26 22:10:03 +0100

    Use custom `QLPreviewItem` that supplies a title too

--- Cobrowse/Helpers/AccountSummaryDataSource.swift
+++ Cobrowse/Helpers/AccountSummaryDataSource.swift
@@ -7,6 +7,20 @@ class AccountSummaryDataSource: NSObject, QLPreviewControllerDataSource {
     }
 
     func previewController(_ controller: QLPreviewController, previewItemAt index: Int) -> any QLPreviewItem {
-        Bundle.main.url(forResource: "account_summary", withExtension: "pdf")! as QLPreviewItem
+        PreviewItem(
+            url: Bundle.main.url(forResource: "account_summary", withExtension: "pdf"),
+            title: "My Summary"
+        )
+    }
+}
+
+private class PreviewItem: NSObject, QLPreviewItem {
+    
+    let previewItemURL: URL?
+    let previewItemTitle: String?
+
+    init(url: URL?, title: String? = nil) {
+        self.previewItemURL = url
+        self.previewItemTitle = title
     }
 }
